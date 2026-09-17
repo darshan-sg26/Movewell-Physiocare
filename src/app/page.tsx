209b49185fc9fb5dcd76e007d84b5aac5f3eb502
darshan-sg26 <darshansg46@gmail.com>
2026-09-17 17:48:46 +0530

    updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -248,7 +248,7 @@ export default function HomePage() {
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {SERVICES_DATA.map((service) => (
-            <ServiceCard key={service.slug} service={service} showImage={false} />
+            <ServiceCard key={service.slug} service={service} />
           ))}
         </div>
       </section>
